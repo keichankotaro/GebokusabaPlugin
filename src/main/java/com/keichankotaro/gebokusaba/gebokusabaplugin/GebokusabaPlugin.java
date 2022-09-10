f5a60eab2f9c0c880ed4068ab8bac15168436ac8
keichankotaro <44016629+keichankotaro@users.noreply.github.com>
2022-09-10 22:31:18 +0900

    Add files via upload
    テレポートに使用するアイテムの種類・個数を指定できるように改良。

--- src/main/java/com/keichankotaro/gebokusaba/gebokusabaplugin/GebokusabaPlugin.java
+++ src/main/java/com/keichankotaro/gebokusaba/gebokusabaplugin/GebokusabaPlugin.java
@@ -10,10 +10,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -46,6 +49,20 @@ public class GebokusabaPlugin extends JavaPlugin{
 		getLogger().info("config.ymlの読み込み中...");
 		saveDefaultConfig();
 		getLogger().info("config.ymlの読み込み完了");
+		
+		/*
+		ItemStack customItem = new ItemStack(Material.*****, 1);
+		ItemMeta customMeta = customItem.getItemMeta();
+		if(customMeta == null) return;
+		customMeta.setDisplayName(ChatColor.AQUA + "******* ***: " + ChatColor.RED + "None");
+		NamespacedKey customKey = new NamespacedKey(this, "storagebox");
+		ShapedRecipe customRecipe = new ShapedRecipe(customKey, customItem);
+		customRecipe.shape("CCC", "CIC", "CCC");
+		customRecipe.setIngredient('C', Material.*****);
+		customRecipe.setIngredient('I', Material.**********);
+		Bukkit.addRecipe(customRecipe);
+		*/
+		
 		getLogger().info("下僕鯖プラグインが起動しました。");
 	}
 	
@@ -92,6 +109,9 @@ public class GebokusabaPlugin extends JavaPlugin{
 			Player player = (Player)sender;
 			Inventory inv = player.getInventory();
 			ItemStack offhand = new ItemStack(player.getInventory().getItemInOffHand());
+			String setitem_normal = getConfig().getString("items.tpalone.item");
+			int setnum_normal = getConfig().getInt("items.tpalone.num");
+			
 			int i = 0;
 			if(offhand.getAmount() != 0) {
 				if(language.equalsIgnoreCase("ja_jp")) {
@@ -100,16 +120,16 @@ public class GebokusabaPlugin extends JavaPlugin{
 					sender.sendMessage("You have an item in your offhand. Remove items from your offhand.");
 				}
 				return true;
-			} else if(inv.contains(Material.IRON_INGOT) == true) {
+			} else if(inv.contains(Material.getMaterial(setitem_normal)) == true) {
 				for (ItemStack item : inv.getContents()) {
-				  if (item != null && item.getType() == Material.IRON_INGOT){
+				  if (item != null && item.getType() == Material.getMaterial(setitem_normal)){
 					  i = i + item.getAmount();
 				  }
 				}
 				
-				if(i >= 2) {
-					inv.remove(Material.IRON_INGOT);
-					inv.addItem(new ItemStack(Material.IRON_INGOT,i-2));
+				if(i >= setnum_normal) {
+					inv.remove(Material.getMaterial(setitem_normal));
+					inv.addItem(new ItemStack(Material.getMaterial(setitem_normal),i-setnum_normal));
 					if(language.equalsIgnoreCase("ja_jp")) {
 						sender.sendMessage(ChatColor.GREEN+"拠点にテレポートします。"+ChatColor.RESET);
 					} else {
@@ -125,16 +145,16 @@ public class GebokusabaPlugin extends JavaPlugin{
 					player.teleport(loc);
 				} else {
 					if(language.equalsIgnoreCase("ja_jp")) {
-						sender.sendMessage(ChatColor.RED+"アイテムが足りません。(不足:"+(2-i)+"鉄インゴット)"+ChatColor.RESET);
+						sender.sendMessage(ChatColor.RED+"アイテムが足りません。(不足:"+(setnum_normal-i)+Material.getMaterial(setitem_normal)+")"+ChatColor.RESET);
 					} else {
-						sender.sendMessage(ChatColor.RED+"Not enough items. (missing: "+(2-i)+" iron ingots)"+ChatColor.RESET);
+						sender.sendMessage(ChatColor.RED+"Not enough items. (missing: "+(setnum_normal-i)+" "+Material.getMaterial(setitem_normal)+")"+ChatColor.RESET);
 					}
 				}
 			} else {
 				if(language.equalsIgnoreCase("ja_jp")) {
-					sender.sendMessage(ChatColor.RED+"アイテムが足りません。(不足:"+(2-i)+"鉄インゴット)"+ChatColor.RESET);
+					sender.sendMessage(ChatColor.RED+"アイテムが足りません。(不足:"+(setnum_normal-i)+Material.getMaterial(setitem_normal)+")"+ChatColor.RESET);
 				} else {
-					sender.sendMessage(ChatColor.RED+"Not enough items. (missing: "+(2-i)+" iron ingots)"+ChatColor.RESET);
+					sender.sendMessage(ChatColor.RED+"Not enough items. (missing: "+(setnum_normal-i)+" "+Material.getMaterial(setitem_normal)+")"+ChatColor.RESET);
 				}
 			}
 			return true;
@@ -154,6 +174,9 @@ public class GebokusabaPlugin extends JavaPlugin{
 			// ここから拠点tp(+エンティティ)
 		} else if(cmd.getName().equalsIgnoreCase("tpbasewithentity")) {
 			
+			String setitem_withentity = getConfig().getString("items.tpwithentity.item");
+			int setnum_withentity = getConfig().getInt("items.tpwithentity.num");
+			
 			Player player = (Player)sender;
 			Inventory inv = player.getInventory();
 			ItemStack offhand = new ItemStack(player.getInventory().getItemInOffHand());
@@ -172,9 +195,9 @@ public class GebokusabaPlugin extends JavaPlugin{
 					sender.sendMessage("You have an item in your offhand. Remove items from your offhand.");
 				}
 				return true;
-			} else if(inv.contains(Material.IRON_INGOT) == true) {
+			} else if(inv.contains(Material.getMaterial(setitem_withentity)) == true) {
 				for (ItemStack item : inv.getContents()) {
-				  if (item != null && item.getType() == Material.IRON_INGOT){
+				  if (item != null && item.getType() == Material.getMaterial(setitem_withentity)){
 					  i = i + item.getAmount();
 				  }
 				}
@@ -185,9 +208,9 @@ public class GebokusabaPlugin extends JavaPlugin{
 					} else {
 						sender.sendMessage(ChatColor.RED+"There are no entities nearby."+ChatColor.RESET);
 					}
-				} else if(i >= 4) {
-					inv.remove(Material.IRON_INGOT);
-					inv.addItem(new ItemStack(Material.IRON_INGOT,i-4));
+				} else if(i >= setnum_withentity) {
+					inv.remove(Material.getMaterial(setitem_withentity));
+					inv.addItem(new ItemStack(Material.getMaterial(setitem_withentity),i-setnum_withentity));
 					if(language.equalsIgnoreCase("ja_jp")) {
 						sender.sendMessage(ChatColor.GREEN+"拠点にテレポートします。"+ChatColor.RESET);
 					} else {
@@ -204,16 +227,16 @@ public class GebokusabaPlugin extends JavaPlugin{
 					player.teleport(loc);
 				} else {
 					if(language.equalsIgnoreCase("ja_jp")) {
-						sender.sendMessage(ChatColor.RED+"アイテムが足りません。(不足:"+(4-i)+"鉄インゴット)"+ChatColor.RESET);
+						sender.sendMessage(ChatColor.RED+"アイテムが足りません。(不足:"+(setnum_withentity-i)+Material.getMaterial(setitem_withentity)+")"+ChatColor.RESET);
 					} else {
-						sender.sendMessage(ChatColor.RED+"Not enough items. (missing: "+(4-i)+" iron ingots)"+ChatColor.RESET);
+						sender.sendMessage(ChatColor.RED+"Not enough items. (missing: "+(setnum_withentity-i)+" "+Material.getMaterial(setitem_withentity)+")"+ChatColor.RESET);
 					}
 				}
 			} else {
 				if(language.equalsIgnoreCase("ja_jp")) {
-					sender.sendMessage(ChatColor.RED+"アイテムが足りません。(不足:"+(4-i)+"鉄インゴット)"+ChatColor.RESET);
+					sender.sendMessage(ChatColor.RED+"アイテムが足りません。(不足:"+(setnum_withentity-i)+Material.getMaterial(setitem_withentity)+")"+ChatColor.RESET);
 				} else {
-					sender.sendMessage(ChatColor.RED+"Not enough items. (missing: "+(4-i)+" iron ingots)"+ChatColor.RESET);
+					sender.sendMessage(ChatColor.RED+"Not enough items. (missing: "+(setnum_withentity-i)+" "+Material.getMaterial(setitem_withentity)+")"+ChatColor.RESET);
 				}
 			} 
 			return true;
@@ -233,5 +256,18 @@ public class GebokusabaPlugin extends JavaPlugin{
 			// ここまでシード値の取得
 		}
 		return false;
+	}
+	
+	@EventHandler
+	public void placeevent(BlockPlaceEvent e) {
+		Player player = e.getPlayer(); // ブロックを置いたプレイヤーを取得する
+		Block block = e.getBlock(); // 設置したブロックを取得
+
+		// メッセージを組み立て
+		StringBuilder builder = new StringBuilder("BlockPlaceEventが発生：");
+		builder.append("設置した物=").append(block.toString()).append(" ");
+
+		// プレイヤーにメッセージを送信
+		player.sendMessage(builder.toString());
 	}
 }
